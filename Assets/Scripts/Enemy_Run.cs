12ec2b281289e69e256565daafdc5e99c7a5b89a
Hugo Vidal <87623017+VidalsHugo@users.noreply.github.com>
2023-07-30 10:42:23 -0300

    Update Enemy_Run.cs

--- Assets/Scripts/Enemy_Run.cs
+++ Assets/Scripts/Enemy_Run.cs
@@ -13,7 +13,6 @@ public class Enemy_Run : StateMachineBehaviour
     EnemyPlus enemyPlus;
 
 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -21,7 +20,6 @@ public class Enemy_Run : StateMachineBehaviour
        enemyPlus = animator.GetComponent<EnemyPlus>();
     }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         enemyPlus.LookAtPlayer();
@@ -36,7 +34,6 @@ public class Enemy_Run : StateMachineBehaviour
         }
     }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
      override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
      {
         animator.ResetTrigger("Attack");
